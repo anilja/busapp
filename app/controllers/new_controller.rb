ee4885a8230caf4da376492755cdd6e72e11c951
trf <trf@ubuntu.ubuntu-domain>
2017-07-24 01:57:57 -0400

    second

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -2,22 +2,8 @@ class NewController < ApplicationController
   def home
   end
 
-  def cabs
-  end
 
   def citybus
-  end
-
-  def metro
-  end
-
-  def bustickets
-  end
-
-  def train
-  end
-
-  def flights
   end
 
 end
